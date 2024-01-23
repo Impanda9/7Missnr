2fe1829bffcf4abe918449548b3dc3a75f0c8972
Impanda9 <137157462+Impanda9@users.noreply.github.com>
2024-01-23 08:52:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("quiz-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const selectedAnswer = document.getElementById('missing-number').value;
+
+  if (selectedAnswer && selectedAnswer.match(/^[0-9]$/)) {
+    localStorage.setItem("MissingNr", selectedAnswer);
+    console.log("Selected answer registered in local storage:", selectedAnswer);
+
+    window.location.href = "8.MissLTR.html";
+  } else {
+    console.log("No answer selected or invalid input");
+  }
+});
+
+const answerInput = document.getElementById('missing-number');
+answerInput.addEventListener('input', function() {
+  if (answerInput.value) {
+    answerInput.classList.add('selected');
+  } else {
+    answerInput.classList.remove('selected');
+  }
+});
